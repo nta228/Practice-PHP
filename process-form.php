839051a22e86ebe00cf67af49685fc0846efc22e
nta228 <64982003+nta228@users.noreply.github.com>
2021-03-08 09:52:39 +0700

    Update process-form.php

--- process-form.php
+++ process-form.php
@@ -1,25 +1,28 @@
-<?php 
- $fullname  = $age = $address ='';
-  if(!empty($_POST)){
-  	if(isset($_POST['fullname'])){
+<?php
+$fullname  = $age = $address = $telephone = '';
+if(!empty($_POST)){
+    if(isset($_POST['fullname'])){
         $fullname = $_POST['fullname'];
-  	}
-  	if(isset($_POST['age'])){
-  		$age = $_POST['age'];
-  	}
-  	if(isset($_POST['address'])){
-  		$address = $_POST['address'];	
-     }
-     
-     if($fullname != '' && $age != ''&& $address != ''){
-	$sql = 'insert into student(fullname, age, address)
-	         values ("'.$fullname.'", "'.$age.'","'.$address.'")';
-	         execute($sql);
-       }
-  	}
-  	if(isset($_GET['id'])){
-	$delete_id = $_GET['id'];
-	$sql = 'delete from student  where id = ' .$delete_id;
-	execute($sql);
-      }	
- ?>
+    }
+    if(isset($_POST['age'])){
+        $age = $_POST['age'];
+    }
+    if(isset($_POST['address'])){
+        $address = $_POST['address'];}
+    if(isset($_POST['telephone'])){
+        $telephone = $_POST['telephone'];}
+
+    }
+
+if($fullname != '' && $age != ''&& $address != '' && $telephone != ''){
+        $sql = 'insert into student(fullname, age, address, telephone )
+	         values ("'.$fullname.'", "'.$age.'","'.$address.'","'.$telephone.'")';
+        execute($sql);
+    }
+}
+if(isset($_GET['id'])){
+    $delete_id = $_GET['id'];
+    $sql = 'delete from student  where id = ' .$delete_id;
+    execute($sql);
+}
+?>
